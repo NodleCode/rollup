225ee6192e723f27bf26141d9cdd454844ccb8a8
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2024-01-09 23:52:21 +0000

    fine tune devccontainer config

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -7,3 +7,7 @@ rustup toolchain install nightly
 
 yarn global add zksync-cli
 cargo +nightly install --git https://github.com/matter-labs/foundry-zksync --force zkcast zkforge
+
+mkdir -p $HOME/.local/share/bash-completion/completions
+zkforge completions bash > $HOME/.local/share/bash-completion/completions/forge
+zkcast completions bash > $HOME/.local/share/bash-completion/completions/cast
