063a32ec0969961a603df96c148aecedc6fa4404
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2024-01-09 15:19:36 -0800

    working tool install

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install --yes pkg-config libssl-dev
+sudo apt install --yes pkg-config libssl-dev libclang-dev docker-compose
 
 rustup toolchain install nightly
 
 yarn global add zksync-cli
-cargo +nightly install --git https://github.com/matter-labs/foundry-zksync --force anvil cast chisel forge zkcast zkforge
+cargo +nightly install --git https://github.com/matter-labs/foundry-zksync --force zkcast zkforge
