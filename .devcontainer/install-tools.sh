b12e8f1e9725642e72764b22e11e9d6b041dda2a
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2024-01-10 11:17:58 -0800

    do not install zkFoundry

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -6,8 +6,8 @@ sudo apt install --yes pkg-config libssl-dev libclang-dev docker-compose
 rustup toolchain install nightly
 
 yarn global add zksync-cli
-cargo +nightly install --git https://github.com/matter-labs/foundry-zksync --force zkcast zkforge
+# cargo +nightly install --git https://github.com/matter-labs/foundry-zksync --force zkcast zkforge
 
-mkdir -p $HOME/.local/share/bash-completion/completions
-zkforge completions bash > $HOME/.local/share/bash-completion/completions/zkforge
-zkcast completions bash > $HOME/.local/share/bash-completion/completions/zkcast
+# mkdir -p $HOME/.local/share/bash-completion/completions
+# zkforge completions bash > $HOME/.local/share/bash-completion/completions/zkforge
+# zkcast completions bash > $HOME/.local/share/bash-completion/completions/zkcast
