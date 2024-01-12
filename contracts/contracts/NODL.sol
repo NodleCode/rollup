1f4531654d6c969f1fda8ddfc8331fc9b8da197f
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2024-01-12 19:21:03 +0000

    token is capped

--- contracts/contracts/NODL.sol
+++ contracts/contracts/NODL.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20CappedUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract NODL is Initializable, ERC20Upgradeable, OwnableUpgradeable {
+contract NODL is Initializable, OwnableUpgradeable, ERC20CappedUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
@@ -15,6 +15,7 @@ contract NODL is Initializable, ERC20Upgradeable, OwnableUpgradeable {
 
     function initialize(address owner) initializer public {
         __ERC20_init("Nodle Token", "NODL");
+        __ERC20Capped_init(21000000000 * (10 ** 18)); // 21B with 18 decimals
         __Ownable_init();
 
         _transferOwnership(owner);
