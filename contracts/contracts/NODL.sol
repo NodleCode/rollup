b6b1dfd41fca02f6707d9e04e0c73a6657f42919
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2024-01-12 03:02:23 +0000

    contract may not use permit since ecrecover is not recommended in zksync

--- contracts/contracts/NODL.sol
+++ contracts/contracts/NODL.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract NODL is Initializable, ERC20Upgradeable, OwnableUpgradeable, ERC20PermitUpgradeable {
+contract NODL is Initializable, ERC20Upgradeable, OwnableUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
@@ -16,7 +16,6 @@ contract NODL is Initializable, ERC20Upgradeable, OwnableUpgradeable, ERC20Permi
     function initialize(address owner) initializer public {
         __ERC20_init("Nodle Token", "NODL");
         __Ownable_init();
-        __ERC20Permit_init("Nodle Token");
 
         _transferOwnership(owner);
     }
