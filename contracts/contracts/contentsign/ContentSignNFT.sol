d4fe54f8438483b140632d4c88a387163590ad4a
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2024-01-13 00:14:32 +0000

    contentsign NFT

---
+++ contracts/contracts/contentsign/ContentSignNFT.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+/// @notice a simple NFT contract for contentsign data where each nft is mapped to a one-time
+/// configurable URL
+contract ContentSignNFT is ERC721, ERC721URIStorage, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    uint256 private _nextTokenId;
+
+    constructor(string memory tokenName, string memory tokenSymbol, address defaultAdmin, address minter) ERC721(tokenName, tokenSymbol) {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, minter);
+    }
+
+    function safeMint(address to, string memory uri) public onlyRole(MINTER_ROLE) {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ contracts/test/contentsign/ContentSignNFT.test.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import { Contract, Wallet } from "zksync-ethers";
+import { getWallet, deployContract, LOCAL_RICH_WALLETS } from '../../deploy/utils';
+import * as ethers from "ethers";
+import exp from 'constants';
+
+describe("ContentSignNFT", function () {
+  let tokenContract: Contract;
+  let ownerWallet: Wallet;
+  let userWallet: Wallet;
+
+  before(async function () {
+    ownerWallet = getWallet(LOCAL_RICH_WALLETS[0].privateKey);
+    userWallet = getWallet(LOCAL_RICH_WALLETS[1].privateKey);
+
+    tokenContract = await deployContract("ContentSignNFT", ["Mock NFT", "MFT", ownerWallet.address, ownerWallet.address], { wallet: ownerWallet, silent: true, skipChecks: true });
+  });
+
+  it("Should mint token", async function () {
+    const tokenURI = "https://example.com";
+
+    await tokenContract.safeMint(userWallet.address, tokenURI);
+    const tokenURIResult = await tokenContract.tokenURI(0);
+
+    expect(tokenURIResult).to.equal(tokenURI);
+  });
+
+  it("Non minter cannot mint token", async function () {
+    const tokenURI = "https://example.com";
+
+    try {
+        await tokenContract.connect(userWallet).safeMint(userWallet.address, tokenURI);
+        expect.fail("Should have reverted");
+    } catch (e) {
+        expect(e.message).to.contain("execution reverted");
+    }
+  });
+});
