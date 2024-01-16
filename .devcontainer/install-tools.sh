39fd4de9281a39a2b039e346c2ce1464af7bbf53
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2024-01-16 20:19:16 +0000

    auto detect zksync version

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install --yes pkg-config build-essential pkg-config cmake clang lldb lld libssl-dev libclang-dev docker-compose postgresql axel software-properties-common
+sudo apt install --yes pkg-config build-essential pkg-config cmake clang lldb lld libssl-dev libclang-dev docker-compose postgresql axel software-properties-common jq
 
 sudo add-apt-repository --yes ppa:ethereum/ethereum
 sudo apt-get update
@@ -17,10 +17,11 @@ yarn global add zksync-cli
 # install and build zk tool
 export ZKSYNC_HOME=/home/$USER/zksync-era
 export PATH=$ZKSYNC_HOME/bin:$PATH
+export VERSION=`./.devcontainer/version.sh matter-labs zksync-era`
 
 git clone https://github.com/matter-labs/zksync-era $ZKSYNC_HOME
 cd $ZKSYNC_HOME
-git checkout core-v19.1.1 # TODO: update to auto select latest release?
+git checkout $VERSION
 mkdir -p $ZKSYNC_HOME/volumes || true
 sudo chown -R $USER:$USER $ZKSYNC_HOME/volumes
 zk
