54d7434b8985a5801b588839a64f2665e4db5dbe
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2024-01-16 19:54:38 +0000

    install tools required for zk stack operations

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install --yes pkg-config libssl-dev libclang-dev docker-compose
+sudo apt install --yes pkg-config build-essential pkg-config cmake clang lldb lld libssl-dev libclang-dev docker-compose postgresql axel software-properties-common
+
+sudo add-apt-repository --yes ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install solc
 
 rustup toolchain install nightly
 
-yarn global add zksync-cli
-# cargo +nightly install --git https://github.com/matter-labs/foundry-zksync --force zkcast zkforge
+cargo install cargo-nextest
+cargo install sqlx-cli
 
-# mkdir -p $HOME/.local/share/bash-completion/completions
-# zkforge completions bash > $HOME/.local/share/bash-completion/completions/zkforge
-# zkcast completions bash > $HOME/.local/share/bash-completion/completions/zkcast
+yarn global add zksync-cli
