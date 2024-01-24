2c16320eb599e8139d4a17b10a631090bd42443e
douglasacost <douglasacostaaquino@gmail.com>
2024-01-24 18:30:32 +0100

    Add factory address to generated YAML files and update deploy script

--- contracts/deploy/deploy.ts
+++ contracts/deploy/deploy.ts
@@ -1,8 +1,13 @@
 import { deployContract, getGovernance, getWhitelistAdmin } from "./utils";
+import { execSync } from "child_process";
 
 export default async function() {
     await deployContract("NODL", [getGovernance(), getGovernance()]);
     
     const factory = await deployContract("ContentSignNFT", ["Click", "CLK", getWhitelistAdmin()]);
-    await deployContract("WhitelistPaymaster", [getWhitelistAdmin(), [await factory.getAddress()]]);
+    const factoryAddress = await factory.getAddress();
+    await deployContract("WhitelistPaymaster", [getWhitelistAdmin(), [factoryAddress]]);
+
+    // run commands with new address, create a file with the address
+    execSync(`echo "${factoryAddress}" > .factory-address`);
 }
---
+++ graph/prepare.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get the address from .factory-address file
+address=$(cat ../contracts/.factory-address)
+
+if [ -z "$address" ]
+then
+      echo "Factory address is empty. Please run yarn deploy-local first."
+      exit 1
+fi
+
+# Replace the old address in the generated/*.yaml files
+echo "Adding address $address to generated/*.yaml files..."
+sed -i "s/address: \".*\"/address: \"$address\"/g" generated/content-sign.subgraph.yaml
+
+# run the build command and deploy command
+yarn build
+echo "Graph build completed. Deploying to local graph node..."
+graph deploy --node http://graph-node:8020/ --ipfs http://ipfs:5001 --version-label v0.0.1 content-sign generated/content-sign.subgraph.yaml
+echo "Graph local deployed."
+
