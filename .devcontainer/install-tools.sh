ff337f4a220ebb7d68e55ab802e1063769c0aa92
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2024-01-30 00:41:06 +0000

    instal less packages

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install --yes pkg-config build-essential pkg-config cmake clang lldb lld libssl-dev libclang-dev docker-compose postgresql axel software-properties-common jq
+sudo apt install --yes pkg-config build-essential cmake clang libssl-dev libclang-dev docker-compose software-properties-common jq
+#sudo apt install --yes lldb lld postgresql axel
 
 sudo add-apt-repository --yes ppa:ethereum/ethereum
 sudo apt-get update
