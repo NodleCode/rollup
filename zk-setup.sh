04461b58b8e63d8e960f4606d6bb36c5639bccb9
douglasacost <douglasacostaaquino@gmail.com>
2024-02-19 16:03:27 +0100

    Update dependencies and environment configurations

--- zk-setup.sh
+++ zk-setup.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
-# Install Docker
-echo "Installing Docker..."
-sudo apt-get update
-sudo apt-get install -y docker.io
-sudo systemctl start docker
-sudo systemctl enable docker
-
-# Add current user to docker group
-sudo usermod -aG docker $(whoami)
-
-# Install Rust
-echo "Installing Rust..."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# Install NVM
-echo "Installing NVM..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-
-# Install build tools and other dependencies
-echo "Installing build tools and other dependencies..."
-sudo apt-get install -y build-essential pkg-config cmake clang lldb lld libssl-dev postgresql axel
-
-# Install Node.js and Yarn
-echo "Installing Node.js and Yarn..."
-source ~/.bashrc  # Reload .bashrc to use nvm
-nvm install 18
-npm install -g yarn
-yarn set version 1.22.19
-
 # Install SQLx CLI
 echo "Installing SQLx CLI..."
 cargo install sqlx-cli --version 0.7.3
@@ -39,27 +10,17 @@ sudo systemctl stop postgresql
 
 echo "Installation complete."
 
-# Clone the zksync-era repo
-echo "Cloning zksync-era repo..."
-git clone https://github.com/matter-labs/zksync-era
-
 # Copy hyperchain-custom.env file
 echo "Copying hyperchain-custom.env file..."
-cp envs/hyperchain-custom.env zksync-era/etc/env/.init.env
-cp envs/hyperchain-custom.env zksync-era/etc/env/
+cp envs/hyperchain-custom.env $ZKSYNC_HOME/etc/env/.init.env
+cp envs/hyperchain-custom.env $ZKSYNC_HOME/etc/env/
 
 #create .current file
 echo "Creating .current file..."
 touch zksync-era/etc/env/.current
 
-cd zksync-era
+cd $ZKSYNC_HOME
 
-# Add ZKSYNC_HOME to your path
-echo "Adding ZKSYNC_HOME to your path..."
-export ZKSYNC_HOME=$(pwd)
-echo "export ZKSYNC_HOME=$ZKSYNC_HOME" >> ~/.bashrc
-echo "export PATH=\$ZKSYNC_HOME/bin:\$PATH" >> ~/.bashrc
-source ~/.bashrc
 
 # Build latest version of zk tools
 echo "Building latest version of zk tools..."
