e0186eff8da5ac11a7b9b066b5d6840736b2d603
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2024-04-02 17:50:05 -0700

    add withdrawing delay to allow to catch security issues

--- test/dot-migration/NODLMigration.t.sol
+++ test/dot-migration/NODLMigration.t.sol
@@ -10,12 +10,13 @@ contract NODLMigrationTest is Test {
     NODLMigration migration;
     NODL nodl;
 
+    uint256 delay = 100;
     address[] oracles = [vm.addr(1), vm.addr(2), vm.addr(3)];
     address user = vm.addr(4);
 
     function setUp() public {
         nodl = new NODL();
-        migration = new NODLMigration(oracles, nodl, 2);
+        migration = new NODLMigration(oracles, nodl, 2, delay);
 
         nodl.grantRole(nodl.MINTER_ROLE(), address(migration));
     }
@@ -25,6 +26,7 @@ contract NODLMigrationTest is Test {
             assertEq(migration.isOracle(oracles[i]), true);
         }
         assertEq(migration.threshold(), 2);
+        assertEq(migration.delay(), delay);
     }
 
     function test_configuredProperToken() public {
@@ -55,6 +57,9 @@ contract NODLMigrationTest is Test {
         vm.prank(oracles[1]);
         migration.bridge(0x0, user, 100);
 
+        vm.roll(block.number + delay + 1);
+        migration.withdraw(0x0);
+
         vm.expectRevert(abi.encodeWithSelector(NODLMigration.AlreadyExecuted.selector, 0x0));
         vm.prank(oracles[2]);
         migration.bridge(0x0, user, 100);
@@ -73,17 +78,82 @@ contract NODLMigrationTest is Test {
         migration.bridge(0x0, oracles[1], 100);
     }
 
-    function test_recordsVotesAndMintTokens() public {
+    function test_recordsVotes() public {
         vm.expectEmit();
         emit NODLMigration.VoteStarted(0x0, oracles[0], user, 100);
         vm.prank(oracles[0]);
         migration.bridge(0x0, user, 100);
 
+        (address target, uint256 amount, uint256 lastVote, uint256 totalVotes, bool executed) = migration.proposals(0x0);
+        assertEq(target, user);
+        assertEq(amount, 100);
+        assertEq(lastVote, block.number);
+        assertEq(totalVotes, 1);
+        assertEq(executed, false);
+
         vm.expectEmit();
         emit NODLMigration.Voted(0x0, oracles[1]);
-        emit NODLMigration.Bridged(0x0, user, 100);
         vm.prank(oracles[1]);
         migration.bridge(0x0, user, 100);
+
+        (target, amount, lastVote, totalVotes, executed) = migration.proposals(0x0);
+        assertEq(target, user);
+        assertEq(amount, 100);
+        assertEq(lastVote, block.number);
+        assertEq(totalVotes, 2);
+        assertEq(executed, false);
+    }
+
+    function test_mayNotWithdrawIfNotEnoughVotes() public {
+        vm.prank(oracles[0]);
+        migration.bridge(0x0, user, 100);
+
+        vm.expectRevert(abi.encodeWithSelector(NODLMigration.NotEnoughVotes.selector, 0x0));
+        migration.withdraw(0x0);
+    }
+
+    function test_mayNotWithdrawBeforeDelay() public {
+        vm.prank(oracles[0]);
+        migration.bridge(0x0, user, 100);
+
+        vm.prank(oracles[1]);
+        migration.bridge(0x0, user, 100);
+
+        vm.expectRevert(abi.encodeWithSelector(NODLMigration.NotYetWithdrawable.selector, 0x0));
+        migration.withdraw(0x0);
+    }
+
+    function test_mayNotWithdrawTwice() public {
+        vm.prank(oracles[0]);
+        migration.bridge(0x0, user, 100);
+
+        vm.prank(oracles[1]);
+        migration.bridge(0x0, user, 100);
+
+        vm.roll(block.number + delay + 1);
+
+        migration.withdraw(0x0);
+
+        vm.expectRevert(abi.encodeWithSelector(NODLMigration.AlreadyExecuted.selector, 0x0));
+        migration.withdraw(0x0);
+    }
+
+    function test_mayWithdrawAfterDelay() public {
+        vm.prank(oracles[0]);
+        migration.bridge(0x0, user, 100);
+
+        vm.prank(oracles[1]);
+        migration.bridge(0x0, user, 100);
+
+        vm.roll(block.number + delay + 1);
+
+        vm.expectEmit();
+        emit NODLMigration.Withdrawn(0x0, user, 100);
+        vm.prank(user); // anybody can call withdraw
+        migration.withdraw(0x0);
+
+        (,,,, bool executed) = migration.proposals(0x0);
+        assertEq(executed, true);
 
         assertEq(nodl.balanceOf(user), 100);
     }
