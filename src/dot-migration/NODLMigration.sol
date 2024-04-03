fba19f3346d6b12affd416fae73108cff69daff7
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2024-04-02 17:31:27 -0700

    bridging contract V1 (#21)
    * fix solc version
    
    * add functions to allow oracle to increase tokens available
    
    * add V1 bridging contract
    
    * support more than one oracle
    
    * clear input verification logic
    
    * implement voting logic
    
    * refactor to be transaction based
    
    * do not check if can execute when just creating a vote
    
    * nb oracles and threshold requirements
    
    * rename contract to be a little clearer

---
+++ src/dot-migration/NODLMigration.sol
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+pragma solidity ^0.8.20;
+
+import {NODL} from "../NODL.sol";
+
+/// @title NODLMigration
+/// @notice This contract is used to help migrating the NODL assets from the Nodle Parachain
+/// to the ZkSync contracts.
+contract NODLMigration {
+    struct Proposal {
+        address target;
+        uint256 amount;
+        uint8 totalVotes;
+        bool executed;
+    }
+
+    NODL public nodl;
+    mapping(address => bool) public isOracle;
+    uint8 public threshold;
+
+    // We track votes in a seperate mapping to avoid having to write helper functions to
+    // expose the votes for each proposal.
+    mapping(bytes32 => Proposal) public proposals;
+    mapping(address => mapping(bytes32 => bool)) public voted;
+
+    error AlreadyVoted(bytes32 proposal, address oracle);
+    error AlreadyExecuted(bytes32 proposal);
+    error ParametersChanged(bytes32 proposal);
+    error NotAnOracle(address user);
+
+    event VoteStarted(bytes32 indexed proposal, address oracle, address indexed user, uint256 amount);
+    event Voted(bytes32 indexed proposal, address oracle);
+    event Bridged(bytes32 indexed proposal, address indexed user, uint256 amount);
+
+    /// @param bridgeOracles Array of oracle accounts that will be able to bridge the tokens.
+    /// @param token Contract address of the NODL token.
+    /// @param minVotes Minimum number of votes required to bridge the tokens. This needs to be
+    /// less than or equal to the number of oracles and is expected to be above 1.
+    constructor(address[] memory bridgeOracles, NODL token, uint8 minVotes) {
+        assert(bridgeOracles.length >= minVotes);
+        assert(minVotes > 1);
+
+        for (uint256 i = 0; i < bridgeOracles.length; i++) {
+            isOracle[bridgeOracles[i]] = true;
+        }
+        nodl = token;
+        threshold = minVotes;
+    }
+
+    /// @notice Bridge some tokens from the Nodle Parachain to the ZkSync contracts. This
+    /// tracks "votes" from each oracle and only bridges the tokens if the threshold is met.
+    /// @param paraTxHash The transaction hash on the Parachain for this transfer.
+    /// @param user The user address.
+    /// @param amount The amount of NODL tokens that the user has burnt on the Parachain.
+    function bridge(bytes32 paraTxHash, address user, uint256 amount) external {
+        _mustBeAnOracle(msg.sender);
+        _mustNotHaveExecutedYet(paraTxHash);
+
+        if (_proposalExists(paraTxHash)) {
+            _mustNotHaveVotedYet(paraTxHash, msg.sender);
+            _mustNotBeChangingParameters(paraTxHash, user, amount);
+            _recordVote(paraTxHash, msg.sender);
+
+            if (_enoughVotes(paraTxHash)) {
+                _mintTokens(paraTxHash, user, amount);
+            }
+        } else {
+            _createVote(paraTxHash, msg.sender, user, amount);
+        }
+    }
+
+    function _mustBeAnOracle(address maybeOracle) internal view {
+        if (!isOracle[maybeOracle]) {
+            revert NotAnOracle(maybeOracle);
+        }
+    }
+
+    function _mustNotHaveVotedYet(bytes32 proposal, address oracle) internal view {
+        if (voted[oracle][proposal]) {
+            revert AlreadyVoted(proposal, oracle);
+        }
+    }
+
+    function _mustNotHaveExecutedYet(bytes32 proposal) internal view {
+        if (proposals[proposal].executed) {
+            revert AlreadyExecuted(proposal);
+        }
+    }
+
+    function _mustNotBeChangingParameters(bytes32 proposal, address user, uint256 amount) internal view {
+        if (proposals[proposal].amount != amount || proposals[proposal].target != user) {
+            revert ParametersChanged(proposal);
+        }
+    }
+
+    function _proposalExists(bytes32 proposal) internal view returns (bool) {
+        return proposals[proposal].totalVotes > 0 && proposals[proposal].amount > 0;
+    }
+
+    function _createVote(bytes32 proposal, address oracle, address user, uint256 amount) internal {
+        voted[oracle][proposal] = true;
+        proposals[proposal].target = user;
+        proposals[proposal].amount = amount;
+        proposals[proposal].totalVotes = 1;
+
+        emit VoteStarted(proposal, oracle, user, amount);
+    }
+
+    function _recordVote(bytes32 proposal, address oracle) internal {
+        voted[oracle][proposal] = true;
+        // this is safe since we are unlikely to have maxUint8 oracles to manage
+        proposals[proposal].totalVotes += 1;
+
+        emit Voted(proposal, oracle);
+    }
+
+    function _enoughVotes(bytes32 proposal) internal view returns (bool) {
+        return proposals[proposal].totalVotes >= threshold;
+    }
+
+    function _mintTokens(bytes32 proposal, address user, uint256 amount) internal {
+        proposals[proposal].executed = true;
+        nodl.mint(user, amount);
+
+        emit Bridged(proposal, user, amount);
+    }
+}
---
+++ test/dot-migration/NODLMigration.t.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {NODLMigration} from "../../src/dot-migration/NODLMigration.sol";
+import {NODL} from "../../src/NODL.sol";
+
+contract NODLMigrationTest is Test {
+    NODLMigration migration;
+    NODL nodl;
+
+    address[] oracles = [vm.addr(1), vm.addr(2), vm.addr(3)];
+    address user = vm.addr(4);
+
+    function setUp() public {
+        nodl = new NODL();
+        migration = new NODLMigration(oracles, nodl, 2);
+
+        nodl.grantRole(nodl.MINTER_ROLE(), address(migration));
+    }
+
+    function test_oraclesAreRegisteredProperly() public {
+        for (uint256 i = 0; i < oracles.length; i++) {
+            assertEq(migration.isOracle(oracles[i]), true);
+        }
+        assertEq(migration.threshold(), 2);
+    }
+
+    function test_configuredProperToken() public {
+        assertEq(address(migration.nodl()), address(nodl));
+    }
+
+    function test_nonOracleMayNotBridgeTokens() public {
+        vm.expectRevert(abi.encodeWithSelector(NODLMigration.NotAnOracle.selector, user));
+        vm.prank(user);
+        migration.bridge(0x0, user, 100);
+    }
+
+    function test_mayNotVoteTwice() public {
+        vm.startPrank(oracles[0]);
+
+        migration.bridge(0x0, user, 100);
+
+        vm.expectRevert(abi.encodeWithSelector(NODLMigration.AlreadyVoted.selector, 0x0, oracles[0]));
+        migration.bridge(0x0, user, 100);
+
+        vm.stopPrank();
+    }
+
+    function test_mayNotVoteOnceExecuted() public {
+        vm.prank(oracles[0]);
+        migration.bridge(0x0, user, 100);
+
+        vm.prank(oracles[1]);
+        migration.bridge(0x0, user, 100);
+
+        vm.expectRevert(abi.encodeWithSelector(NODLMigration.AlreadyExecuted.selector, 0x0));
+        vm.prank(oracles[2]);
+        migration.bridge(0x0, user, 100);
+    }
+
+    function test_mayNotVoteIfParametersChanged() public {
+        vm.prank(oracles[0]);
+        migration.bridge(0x0, user, 100);
+
+        vm.expectRevert(abi.encodeWithSelector(NODLMigration.ParametersChanged.selector, 0x0));
+        vm.prank(oracles[1]);
+        migration.bridge(0x0, user, 200);
+
+        vm.expectRevert(abi.encodeWithSelector(NODLMigration.ParametersChanged.selector, 0x0));
+        vm.prank(oracles[1]);
+        migration.bridge(0x0, oracles[1], 100);
+    }
+
+    function test_recordsVotesAndMintTokens() public {
+        vm.expectEmit();
+        emit NODLMigration.VoteStarted(0x0, oracles[0], user, 100);
+        vm.prank(oracles[0]);
+        migration.bridge(0x0, user, 100);
+
+        vm.expectEmit();
+        emit NODLMigration.Voted(0x0, oracles[1]);
+        emit NODLMigration.Bridged(0x0, user, 100);
+        vm.prank(oracles[1]);
+        migration.bridge(0x0, user, 100);
+
+        assertEq(nodl.balanceOf(user), 100);
+    }
+}
