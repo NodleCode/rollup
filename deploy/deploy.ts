7fa77f6375fe54bf7cff3c027e919460f563555e
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2024-04-11 13:22:25 -0700

    add extra logging

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -8,14 +8,19 @@ export default async function () {
     const paymaster = await deployContract("WhitelistPaymaster", [withdrawer]);
     const nft = await deployContract("ContentSignNFT", ["ContentSign", "SIGNED", await paymaster.getAddress()]);
 
+    console.log("WhitelistPaymaster deployed to:", await paymaster.getAddress());
+    console.log("ContentSignNFT deployed to:", await nft.getAddress());
+
+    console.log("Adding NFT contract to whitelist...");
     await paymaster.addWhitelistedContracts([await nft.getAddress()]);
 
+    console.log("Granting roles to correct admins...");
     const role_whitelist_admin = await paymaster.WHITELIST_ADMIN_ROLE();
     const role_super_admin = await paymaster.DEFAULT_ADMIN_ROLE();
-
     await paymaster.grantRole(role_whitelist_admin, whitelist_admin);
     await paymaster.grantRole(role_super_admin, super_admin);
 
+    console.log("Revoking roles from deployer...");
     const our_address = (await getWallet()).address;
     await paymaster.renounceRole(role_whitelist_admin, our_address);
     await paymaster.renounceRole(role_super_admin, our_address);
