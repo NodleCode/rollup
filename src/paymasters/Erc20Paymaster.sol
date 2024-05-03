73ddc3be3e20979db0982fb87468f123699a1bb0
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2024-05-03 10:31:39 -0700

    fix license header

--- src/paymasters/Erc20Paymaster.sol
+++ src/paymasters/Erc20Paymaster.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BSD-3-Clause-Clear
 pragma solidity 0.8.23;
 
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
