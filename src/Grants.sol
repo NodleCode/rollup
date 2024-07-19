c7d9c808f9b3e0a707901f56c2db8885380d56b6
Alex Sedighi <alex@nodle.com>
2024-07-19 06:16:36 +0000

    fix claim and cancel

--- src/Grants.sol
+++ src/Grants.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.23;
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract VestingContract {
+contract Grants {
     using SafeERC20 for IERC20;
 
     struct VestingSchedule {
@@ -56,8 +56,9 @@ contract VestingContract {
         uint256 totalClaimable = 0;
         uint256 currentTime = block.timestamp;
 
+        uint256 i = 0;
         VestingSchedule[] storage schedules = vestingSchedules[msg.sender];
-        for (uint256 i = 0; i < schedules.length; i++) {
+        while (i < schedules.length) {
             VestingSchedule storage schedule = schedules[i];
             if (currentTime > schedule.start) {
                 uint256 periodsElapsed = (currentTime - schedule.start) / schedule.period;
@@ -69,13 +70,14 @@ contract VestingContract {
                 if (schedule.periodCount == 0) {
                     schedules[i] = schedules[schedules.length - 1];
                     schedules.pop();
-                    i--;
+                    continue;
                 }
             }
+            i++;
         }
 
         if (totalClaimable > 0) {
-            token.safeTransferFrom(address(this), msg.sender, totalClaimable);
+            token.safeTransfer(msg.sender, totalClaimable);
         }
         emit Claimed(msg.sender, totalClaimable);
     }
@@ -92,11 +94,13 @@ contract VestingContract {
         uint256 totalRedeemable = 0;
         uint256 currentTime = block.timestamp;
 
+        uint256 i = 0;
         VestingSchedule[] storage schedules = vestingSchedules[to];
-        for (uint256 i = 0; i < schedules.length; i++) {
+        while (i < schedules.length) {
             VestingSchedule storage schedule = schedules[i];
             if (schedule.from == msg.sender) {
-                uint256 periodsElapsed = (currentTime - schedule.start) / schedule.period;
+                uint256 periodsElapsed =
+                    currentTime > schedule.start ? (currentTime - schedule.start) / schedule.period : 0;
                 uint256 effectivePeriods = periodsElapsed > schedule.periodCount ? schedule.periodCount : periodsElapsed;
                 uint256 claimable = effectivePeriods * schedule.perPeriodAmount;
                 uint256 redeemable = (schedule.periodCount - effectivePeriods) * schedule.perPeriodAmount;
@@ -104,19 +108,24 @@ contract VestingContract {
                 totalRedeemable += redeemable;
                 schedules[i] = schedules[schedules.length - 1];
                 schedules.pop();
-                i--;
+                continue;
             }
+            i++;
         }
 
         if (totalClaimable > 0) {
-            token.safeTransferFrom(address(this), to, totalClaimable);
+            token.safeTransfer(to, totalClaimable);
         }
 
         if (totalRedeemable > 0) {
-            token.safeTransferFrom(address(this), msg.sender, totalRedeemable);
+            token.safeTransfer(msg.sender, totalRedeemable);
         }
 
         emit VestingSchedulesCanceled(msg.sender, to);
+    }
+
+    function getGrantsCount(address to) external view returns (uint256) {
+        return vestingSchedules[to].length;
     }
 
     function _mustBeNonZero(uint256 value) private pure {
