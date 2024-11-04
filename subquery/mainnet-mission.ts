6cf396ce54705989f0de4e52f69bdbf0fd2960ed
douglasacost <douglasacostaaquino@gmail.com>
2024-11-04 18:47:09 +0100

    Add mainnet-mission subquery configuration

---
+++ subquery/mainnet-mission.ts
@@ -0,0 +1,103 @@
+require("dotenv").config();
+import {
+  EthereumProject,
+  EthereumDatasourceKind,
+  EthereumHandlerKind,
+} from "@subql/types-ethereum";
+
+const project: EthereumProject = {
+  specVersion: "1.0.0",
+  version: "0.0.1",
+  name: "Nodle-zksync-subquery",
+  description: "__",
+  runner: {
+    node: {
+      name: "@subql/node-ethereum",
+      version: ">=3.0.0",
+      options: {
+        unsafe: true,
+      },
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    chainId: "324", // zKsync mainnet
+    endpoint: [
+      process.env.ZKSYNC_MAINNET_RPC!,
+      "https://mainnet.era.zksync.io",
+    ],
+  },
+  dataSources: [
+    {
+      kind: EthereumDatasourceKind.Runtime,
+      startBlock: 33999048, // This is the block that the contract was deployed on
+      options: {
+        abi: "NODL",
+        address: "0xBD4372e44c5eE654dd838304006E1f0f69983154",
+      },
+      assets: new Map([
+        [
+          "NODL",
+          {
+            file: "./abis/erc20.abi.json",
+          },
+        ],
+      ]),
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          {
+            kind: EthereumHandlerKind.Event,
+            handler: "handleERC20Transfer",
+            filter: {
+              topics: ["Transfer(address,address,uint256)"],
+            },
+          },
+        ],
+      },
+    },
+    {
+      kind: EthereumDatasourceKind.Runtime,
+      startBlock: 31551522, // This is the block that the contract was deployed on
+      options: {
+        abi: "erc721",
+        address: "0x95b3641d549f719eb5105f9550Eca4A7A2F305De",
+      },
+      assets: new Map([
+        [
+          "erc721",
+          {
+            file: "./abis/erc721.abi.json",
+          },
+        ],
+      ]),
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          {
+            kind: EthereumHandlerKind.Call,
+            handler: "handleSafeMint",
+            filter: {
+              function: "safeMint(address,string)",
+            },
+          },
+          {
+            kind: EthereumHandlerKind.Event,
+            handler: "handleTransfer",
+            filter: {
+              topics: ["Transfer(address,address,uint256)"],
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
+
+export default project;
