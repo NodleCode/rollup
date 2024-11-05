519464540a9dc46024b7082e746e77f7d4adfb7c
douglasacost <douglasacostaaquino@gmail.com>
2024-11-05 14:53:56 +0100

    Restore nodl.ts mapping

--- subquery/src/mappings/nodl.ts
+++ subquery/src/mappings/nodl.ts
@@ -1,14 +1,8 @@
 import { fetchContract } from "../utils/erc20";
 import { ApprovalLog, TransferLog } from "../types/abi-interfaces/NODLAbi";
 import { fetchAccount, fetchTransaction } from "../utils/utils";
-import {
-  ERC20Approval,
-  ERC20Transfer,
-  ERC20TransferV2,
-  Wallet,
-} from "../types";
-import { handleSnapshot } from "../utils/snapshot";
-import { abi, callContract } from "../utils/const";
+import { ERC20Approval, ERC20Transfer, ERC20TransferV2, Wallet } from "../types";
+import { handleSnapshot, handleStatSnapshot } from "../utils/snapshot";
 
 export async function handleERC20Transfer(event: TransferLog): Promise<void> {
   if (!event.args) {
@@ -70,43 +64,24 @@ export async function handleERC20Transfer(event: TransferLog): Promise<void> {
       value
     );
 
-    const blockNumber = "0x" + (event.block.number).toString(16);
-
-    const fromBalance = callContract(
-      contract.id,
-      abi,
-      "balanceOf",
-      [from.id],
-      blockNumber
-    );
-    const toBalance = callContract(
-      contract.id,
-      abi,
-      "balanceOf",
-      [to.id],
-      blockNumber
-    );
-    const [fromBalanceValue, toBalanceValue] = await Promise.all([
-      fromBalance,
-      toBalance,
-    ]);
-    from.balance = BigInt(String(fromBalanceValue));
-    to.balance = BigInt(String(toBalanceValue));
+    from.balance =
+      (from.balance || BigInt(0)) - (value > 0 ? value : BigInt(0));
+    to.balance = (to.balance || BigInt(0)) + value;
 
     await Promise.all([
-      // lightTransfer.save(),
+      lightTransfer.save(),
       from.save(),
       to.save(),
       handleSnapshot(event, from, value),
       handleSnapshot(event, to, BigInt(0)),
-      // handleStatSnapshot(timestamp, value, BigInt(0), newWallet),
+      handleStatSnapshot(timestamp, value, BigInt(0), newWallet),
     ]);
 
     transferEvent.hash = hash;
     transferEvent.contractId = contract.id;
     transferEvent.emitterId = emitter.id;
     //logger.info("Saving transferEvent");
-    // return transferEvent.save();
+    return transferEvent.save();
   }
 }
 
