38a47c5e53d888437289d474b9140f575bb6c297
Alex Sedighi <alex@nodle.com>
2024-11-18 15:06:20 +1300

    feat: deploy storageProofVerifier

--- script/DeployL1Ens.s.sol
+++ script/DeployL1Ens.s.sol
@@ -3,25 +3,37 @@ pragma solidity ^0.8.18;
 
 import {Script, console} from "lib/forge-std/src/Script.sol";
 import {SparseMerkleTree} from "lib/zksync-storage-proofs/packages/zksync-storage-contracts/src/SparseMerkleTree.sol";
+import {
+    StorageProofVerifier,
+    IZkSyncDiamond
+} from "lib/zksync-storage-proofs/packages/zksync-storage-contracts/src/StorageProofVerifier.sol";
 
 /// This contract is used to deploy the all the L1 contracts that Nodle needs to allow clk.eth subdomains to be created and resoved on L2
 contract DeployL1Ens is Script {
-    address public spvAddress;
-
     function run() external {
-        string memory smtDefault = "";
-        string memory smtAddress = vm.envOr("SPARSE_MERKLE_TREE_ADDR", smtDefault);
         string memory deployerPrivateKey = vm.envString("DEPLOYER_PRIVATE_KEY");
-
         vm.startBroadcast(vm.parseUint(deployerPrivateKey));
 
-        if (bytes(smtAddress).length == 0) {
-            console.log("Deploying SparseMerkleTree...");
-            SparseMerkleTree sparseMerkleTree = new SparseMerkleTree();
-            spvAddress = address(sparseMerkleTree);
-            console.log("Deployed SparseMerkleTree at", smtAddress);
+        address spvAddress = vm.envOr("STORAGE_PROOF_VERIFIER_ADDR", address(0));
+
+        if (spvAddress == address(0)) {
+            address smtAddress = vm.envOr("SPARSE_MERKLE_TREE_ADDR", address(0));
+            if (smtAddress == address(0)) {
+                console.log("Deploying SparseMerkleTree...");
+                SparseMerkleTree sparseMerkleTree = new SparseMerkleTree();
+                smtAddress = address(sparseMerkleTree);
+                console.log("Deployed SparseMerkleTree at", smtAddress);
+            } else {
+                console.log("Using SparseMerkleTree at", smtAddress);
+            }
+
+            console.log("Deploying StorageProofVerifier...");
+            StorageProofVerifier storageProofVerifier = new StorageProofVerifier(
+                IZkSyncDiamond(vm.envAddress("DIAMOND_PROXY_ADDR")), SparseMerkleTree(smtAddress)
+            );
+            spvAddress = address(storageProofVerifier);
         } else {
-            console.log("Using SparseMerkleTree at", smtAddress);
+            console.log("Using StorageProofVerifier at", spvAddress);
         }
 
         vm.stopBroadcast();
