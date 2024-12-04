227f019ab91e5a86aff70c7af3540057113e0c70
Alex Sedighi <alex@nodle.com>
2024-12-04 14:50:10 +1300

    feat(ClickResolver): simplify resolver

--- src/nameservice/ClickResolver.sol
+++ src/nameservice/ClickResolver.sol
@@ -14,15 +14,6 @@ import {
     StorageProofVerifier
 } from "zksync-storage-proofs/packages/zksync-storage-contracts/src/StorageProofVerifier.sol";
 
-/// @title IOffchainResolver
-/// @dev Expected interface for offchain resolvers which are contacted after an OffchainLookup revert
-interface IOffchainResolver {
-    function resolve(bytes calldata name, bytes calldata data)
-        external
-        view
-        returns (StorageProof memory proof, bytes32 fallbackValue);
-}
-
 /// @title IExtendedResolver
 /// @notice ENSIP-10: Wildcard Resolution
 interface IExtendedResolver {
@@ -41,8 +32,6 @@ contract ClickResolver is IExtendedResolver, IERC165, Ownable {
     error UnsupportedSelector(bytes4 selector);
     error UnsupportedChain(uint256 coinType);
     error InvalidStorageProof();
-    error InvalidSignature();
-    error InvalidDnsDomain();
 
     /// @notice Storage proof verifier contract
     StorageProofVerifier public storageProofVerifier;
@@ -50,20 +39,17 @@ contract ClickResolver is IExtendedResolver, IERC165, Ownable {
     /// @notice URL of the resolver
     string public url;
 
-    /// @notice Address of the registry contract on L2
-    address public registry;
+    /// @notice Address of the register contract on L2
+    address public immutable registry;
 
-    /// @notice Storage slot for the mapping index, specific to Registry contract
-    uint256 public mappingSlot;
+    /// @notice Storage slot for the mapping index, specific to registry contract
+    uint256 public immutable mappingSlot;
 
     /// @notice Address of the domain owner
     address public domainOwner;
 
-    /// @notice If true, proofs will be validated to make sure they are correct
-    bool public validateProofs = true;
-
     constructor(string memory _url, address _domainOwner, address _registry, StorageProofVerifier _storageProofVerifier)
-        Ownable(msg.sender)
+        Ownable(_domainOwner)
     {
         url = _url;
         domainOwner = _domainOwner;
@@ -74,44 +60,15 @@ contract ClickResolver is IExtendedResolver, IERC165, Ownable {
         mappingSlot = 2;
     }
 
-    function setValidate(bool _validate) external onlyOwner {
-        validateProofs = _validate;
-    }
-
     function setUrl(string memory _url) external onlyOwner {
         url = _url;
     }
 
-    function setRegistry(address _registry, uint256 _mappingSlot) external onlyOwner {
-        registry = _registry;
-        mappingSlot = _mappingSlot;
-    }
-
-    /**
-     * @notice Sets the mapping slot of the corresponding registry contract that could be enquired for a proof.
-     * @dev This should be set based on the position of the `_owners` storage of ClickNameResolver coming from its ERC721 base.
-     *      The storage-layout of that contract should be consulted to determine the correct slot.
-     * @param _mappingSlot The slot to be set for the mapping.
-     */
-    function setMappingSlot(uint256 _mappingSlot) external onlyOwner {
-        mappingSlot = _mappingSlot;
-    }
-
-    function setStorageProofVerifier(StorageProofVerifier _storageProofVerifier) external onlyOwner {
-        storageProofVerifier = _storageProofVerifier;
-    }
-
-    /// @notice Extract namehash from calldata
-    function extractNamehash(bytes calldata data) public pure returns (bytes32 namehash) {
-        // Cast last 32 bytes of data to bytes32
-        namehash = bytes32(data[data.length - 32:]);
-    }
-
     /// @notice Parses DNS encoded domain name
     /// @param name DNS encoded domain name
-    /// @return sub Subdomain
-    /// @return dom Domain
-    /// @return top Top level domain
+    /// @return _sub Subdomain
+    /// @return _dom Domain
+    /// @return _top Top level domain
     /// @dev e.g example.clave.eth is encoded as b"\x07example\x05clave\x03eth"
     ///      sub = "example"
     ///      dom = "clave"
@@ -121,7 +78,7 @@ contract ClickResolver is IExtendedResolver, IERC165, Ownable {
     function parseDnsDomain(bytes calldata name)
         internal
         pure
-        returns (string memory sub, string memory dom, string memory top)
+        returns (string memory _sub, string memory _dom, string memory _top)
     {
         uint256 length = name.length;
 
@@ -145,59 +102,32 @@ contract ClickResolver is IExtendedResolver, IERC165, Ownable {
         return (first, second, third);
     }
 
-    /// @notice Calculates storage slot of the key in the L2 registry
-    /// @dev Names are stored in the L2 registry, in a mapping with slot `mappingSlot`
-    function getDomainSlot(bytes32 _key) public view returns (bytes32) {
-        return keccak256(abi.encode(_key, mappingSlot));
+    /// @notice Calculates the key for the given subdomain name in the L2 registry
+    /// @dev Names are stored in the registry, in a mapping with slot `mappingSlot`
+    function getStorageKey(string memory subDomain) public view returns (uint256) {
+        uint256 tokenId = uint256(keccak256(abi.encodePacked(subDomain)));
+        return uint256(keccak256(abi.encode(tokenId, mappingSlot)));
     }
 
-    /// @notice Helper function to convert a string to lowercase
-    function toLower(string memory str) private pure returns (string memory) {
-        bytes memory bStr = bytes(str);
-        bytes memory bLower = new bytes(bStr.length);
-        for (uint256 i = 0; i < bStr.length; i++) {
-            // Uppercase character...
-            if ((uint8(bStr[i]) >= 65) && (uint8(bStr[i]) <= 90)) {
-                // So we add 32 to make it lowercase
-                bLower[i] = bytes1(uint8(bStr[i]) + 32);
-            } else {
-                bLower[i] = bStr[i];
-            }
-        }
-        return string(bLower);
-    }
-
-    /// @notice Calculates storage slot of the key in the L2 registry
-    /// @dev Names are stored in the L2 registry, in a mapping with slot `mappingSlot`
-    function getDomainSlot(string memory _key) public view returns (bytes32) {
-        string memory domain = toLower(_key);
-        uint256 tokenId = uint256(keccak256(abi.encodePacked(domain)));
-        return keccak256(abi.encode(tokenId, mappingSlot));
-    }
-
-    /// @notice Resolves a name to a value
-    /// @param _name The name to resolve, DNS encoded
+    /// @notice Resolves a name based on its subdomain part regardless of the given domain and top level
+    /// @param _name The name to resolve which must be a pack of length prefixed names for subdomain, domain and top. 
+    /// example: b"\x07example\x05clave\x03eth"
+    ///
     /// @param _data The ABI encoded data for the underlying resolution function (Eg, addr(bytes32), text(bytes32,string), etc).
     function resolve(bytes calldata _name, bytes calldata _data) external view returns (bytes memory) {
-        bytes memory callData = abi.encodeWithSelector(IOffchainResolver.resolve.selector, _name, _data);
+        (string memory sub, string memory _domain, string memory _top) = parseDnsDomain(_name);
+
+        if (bytes(sub).length == 0) {
+            return abi.encodePacked(domainOwner);
+        }
+
+        bytes memory callData = abi.encode(sub);
 
         // Fill URLs
         string[] memory urls = new string[](1);
         urls[0] = url;
 
-        (string memory sub, string memory dom, string memory top) = parseDnsDomain(_name);
-
-        // If there's no domain or top level domain, throw
-        if (bytes(dom).length == 0 || bytes(top).length == 0) {
-            revert InvalidDnsDomain();
-        }
-
-        if (bytes(sub).length == 0) {
-            // If there's no subdomain, return the domain owner
-            return abi.encodePacked(domainOwner);
-        }
-
-        bytes32 registrySlot = getDomainSlot(sub);
+        uint256 key = getStorageKey(sub);
 
         bytes4 functionSelector = bytes4(_data[:4]);
         if (functionSelector == ADDR_SELECTOR) {
@@ -206,7 +136,7 @@ contract ClickResolver is IExtendedResolver, IERC165, Ownable {
                 urls,
                 callData,
                 ClickResolver.resolveWithProof.selector,
-                abi.encode(registry, registrySlot)
+                abi.encode(key)
             );
         } else if (functionSelector == ADDR_MULTICHAIN_SELECTOR) {
             (, uint256 coinType) = abi.decode(_data[4:], (bytes32, uint256));
@@ -219,7 +149,7 @@ contract ClickResolver is IExtendedResolver, IERC165, Ownable {
                 urls,
                 callData,
                 ClickResolver.resolveWithProof.selector,
-                abi.encode(registry, registrySlot)
+                abi.encode(key)
             );
         } else {
             revert UnsupportedSelector(functionSelector);
@@ -230,27 +160,21 @@ contract ClickResolver is IExtendedResolver, IERC165, Ownable {
     /// @param _response ABI encoded StorageProof struct
     /// @return ABI encoded value of the storage key
     function resolveWithProof(bytes memory _response, bytes memory _extraData) external view returns (bytes memory) {
-        (StorageProof memory proof, bytes32 fallbackValue) = abi.decode(_response, (StorageProof, bytes32));
-        (address account, uint256 key) = abi.decode(_extraData, (address, uint256));
+        (StorageProof memory proof) = abi.decode(_response, (StorageProof));
+        (uint256 key) = abi.decode(_extraData, (uint256));
 
-        if (validateProofs) {
-            // Override account and key of the proof to make sure it is correct address and key
-            proof.account = account;
-            proof.key = key;
-        }
+        // Replace the account in the proof with the known address of the registry
+        proof.account = registry;
+        // Replace the key in the proof with the caller's specified key. It's because the caller may obtain the response/proof from an untrusted offchain source.
+        proof.key = key;
 
         bool verified = storageProofVerifier.verify(proof);
 
-        // If there's an address for the name, this should be an address
-        // But example implementation is returning bytes and we're doing the same
-        if (verified && proof.value != bytes32(0)) {
-            return abi.encodePacked(proof.value);
-        } else {
-            // After username is set on L2, there'll be a time period where
-            // the username is not yet set on L1. During this time, the username
-            // will be set to 0x00. In this case, return the fallback value.
-            return abi.encodePacked(fallbackValue);
+        if (!verified) {
+            revert InvalidStorageProof();
         }
+
+        return abi.encodePacked(proof.value);
     }
 
     /**
