c970a96cb2286784ba356f5123a4079a5c521e54
douglasacost <douglasacostaaquino@gmail.com>
2024-12-09 11:57:48 +0100

    Refactor handleNameRegistered to improve argument handling and variable naming

--- subquery/src/mappings/ens.ts
+++ subquery/src/mappings/ens.ts
@@ -1,27 +1,29 @@
-import { NameRegisteredLog } from './../types/abi-interfaces/ENSAbi';
-import { fetchAccount } from '../utils/utils';
-import { ENS } from '../types';
+import { NameRegisteredLog } from "./../types/abi-interfaces/ENSAbi";
+import { fetchAccount } from "../utils/utils";
+import { ENS } from "../types";
 
-export async function handleNameRegistered(event: NameRegisteredLog): Promise<void> {
+export async function handleNameRegistered(
+  event: NameRegisteredLog
+): Promise<void> {
   if (!event.args) {
     logger.error("No event.args");
     return;
   }
 
+  const [name, owner, expires] = event.args;
   const timestamp = event.block.timestamp * BigInt(1000);
-  const expiresAt =
-    event.args.expires ? event.args.expires.toBigInt() * BigInt(1000) : BigInt(0);
-  const owner = await fetchAccount(event.args.owner, timestamp);
-  const name = event.args.name.toString();
+  const _expires = expires ? expires.toBigInt() * BigInt(1000) : BigInt(0);
+  const _owner = await fetchAccount(owner, timestamp);
+  const _name = event.args.name.toString();
   const txHash = event.transaction.hash;
 
   const registeredEns = new ENS(
     txHash.concat("/").concat(name),
-    owner.id,
-    expiresAt,
+    _owner.id,
+    _expires,
     timestamp,
-    name
+    _name
   );
-  
+
   return registeredEns.save();
 }
